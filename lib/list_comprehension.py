c94497ece08c1987bd4408f8ff1164f4b477239a
Mayowa Ajiboye <mayaajiboye@gmail.com>
2023-02-24 12:49:00 -0500

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
+# spot = [returned pet/loop/conditional]
+
+
 
 def return_evens(num_list):
-    pass
+    num_list = [num for num in num_list if num % 2 == 0]
+    return(num_list)
+
 
 def make_exclamation(sentence_list):
-    pass
+    new_list = [list + '!' for list in sentence_list]
+    return(new_list)
